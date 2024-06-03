2974277fb307551bba9ad7101bf65addebf5bf8c
je <wjddmsy12345@gmail.com>
2024-06-03 17:05:59 +0900

    0603
    2024.06.03월

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -6,6 +6,9 @@ import {
   setDoc,
   doc,
   addDoc,
+  deleteDoc,
+  getDoc,
+  updateDoc,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -46,5 +49,18 @@ async function addDatas(collectionName, dataObj) {
     return fales;
   }
 }
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+}
 
-export { db, getDatas, addDatas };
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  // doc(db,컬렉션명, 문서ID);
+  // getDocs(문서레퍼런스);
+  // updateDoc(문서데이터, 수정할 정보객체);
+  const docRef = await doc(db, collectionName, docId);
+  // const docData = await getDoc(docRef);
+  await updateDoc(docRef, updateInfoObj);
+}
+
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
