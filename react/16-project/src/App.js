773e2e54258a8b9ee2d76dfd6941e04567deef0b
je <wjddmsy12345@gmail.com>
2024-08-19 22:03:34 +0900

    .
    .

---
+++ react/16-project/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CompanyPage from "./CompanyPage";
+
+function App() {
+  return (
+    <div className="App">
+      <CompanyPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/16-project/src/CompanyPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./CompanyPage.module.scss";
+function CompanyPage(props) {
+  return (
+    <div>
+      <div className={styles.title}>
+        <h1>아이팜 ......</h1>
+        <p>IT 기술과 데이터로 농식품 .....</p>
+      </div>
+      <div>
+        <div>
+          <h2>VISION</h2>
+          <h2>비전</h2>
+          <p>아이팜은 지속적인 도약과 성장으로 기존 비즈니스 역량....</p>
+          <div>
+            <img />
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CompanyPage;
