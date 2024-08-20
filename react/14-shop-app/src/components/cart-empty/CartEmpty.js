3eeffe687e990484dd930adfa940746f69336a28
je <wjddmsy12345@gmail.com>
2024-08-20 11:13:40 +0900

    .

--- react/14-shop-app/src/components/cart-empty/CartEmpty.js
+++ react/14-shop-app/src/components/cart-empty/CartEmpty.js
@@ -6,6 +6,7 @@ function CartEmpty({ title }) {
   return (
     <div className={styles.cart_empty}>
       <img src="img/empty-cart.png" />
+      <img src="img/iformlogo.png" className={styles.logo} />
       <h1>{title}(가)이 비어있습니다.</h1>
       <p>{title}에 상품을 넣어주세요.</p>
       <Link to={"/"}>계속 쇼핑하기</Link>
