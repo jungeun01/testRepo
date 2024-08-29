42341139ebea030e814c6998367058086b71c019
je <wjddmsy12345@gmail.com>
2024-08-29 22:01:30 +0900

    .

--- react/18-dashboard-sty/src/App.js
+++ react/18-dashboard-sty/src/App.js
@@ -1,21 +1,56 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
   const big = ["과수", "사료", "수목"];
-  const middle1 = ["과채류", "소과류", "중과류"];
-  const middle2 = ["바보", "멍충", "퉤"];
-  const middle3 = ["??", "하기싫", "ㅇㄴㄹ"];
-  const small = ["딸기", "참외", "파프리카"];
 
-  const handleSlect = (e) => {
-    console.log(e.target.value);
+  const middle = {
+    과수: ["견과류", "소과류", "열대과수"],
+    사료: ["사료작물"],
+    수목: ["수목작물", "수목나무"],
+  };
+  const small = {
+    견과류: ["딸기"],
+    소과류: ["귤", "바나나"],
+    열대과수: ["망고", "파인애플"],
+    사료작물: ["옥수수", "알팔파"],
+    수목작물: ["소나무", "전나무"],
+    수목나무: ["참나무", "단풍나무"],
+  };
+
+  // const middle1 = ["견과류", "소과류", "열대과수"];
+  // const middle2 = ["사료작물"];
+  // const middle3 = ["수목"];
+  // const small1 = ["딸기", "참외", "파프리카"];
+  // const small2 = ["밤", "귤", "바나나"];
+  // const small3 = ["가지", "복숭아", "고구마"];
+
+  const [selectedBig, setSelectedBig] = useState(""); // 첫 번째 셀렉트 선택 값
+  const [middleOptions, setMiddleOptions] = useState([]); // 두 번째 셀렉트 박스 옵션
+  const [selectedMiddle, setSelectedMiddle] = useState(""); // 두 번째 셀렉트 선택 값
+  const [smallOptions, setSmallOptions] = useState([]); // 세 번째 셀렉트 박스 옵션
+
+  // 첫 번쨰 셀렉트 박스 변경 핸들러
+  const handleBigChang = (e) => {
+    const value = e.target.value;
+    setSelectedBig(value);
+    setMiddleOptions(middle[value] || []);
+    setSelectedMiddle("");
+    setSmallOptions([]);
+  };
+
+  //  두번째 셀렉트 박스 핸들러
+  const handleMiddleChange = (e) => {
+    const value = e.target.value;
+    setSelectedMiddle(value);
+    setSmallOptions(small[value] || []);
   };
 
   return (
     <div className="App">
       <div>
         <span>작물</span>
-        <select onChange={handleSlect}>
+        {/* 첫번쨰 셀렉트박스 */}
+        <select onChange={handleBigChang}>
           <option value="" title="대분류">
             ::전체::
           </option>
@@ -25,17 +60,22 @@ function App() {
             </option>
           ))}
         </select>
-        <select onChange={handleSlect}>
+
+        {/* 두번쨰 셀렉트박스 */}
+
+        <select onChange={handleMiddleChange}>
           <option value="">::전체::</option>
-          {middle1.map((item, idx) => (
+          {middleOptions.map((item, idx) => (
             <option key={idx} value={item}>
               {item}
             </option>
           ))}
         </select>
-        <select onChange={handleSlect}>
+        {/* 세번쨰 셀렉트박스 */}
+
+        <select>
           <option value="">::전체::</option>
-          {small.map((item, idx) => (
+          {smallOptions.map((item, idx) => (
             <option key={idx} value={item}>
               {item}
             </option>
